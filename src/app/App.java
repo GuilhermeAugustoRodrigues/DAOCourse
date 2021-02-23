34b841efd0ce79d98bbde442987c7680ef2d83ad
Guilhermino <Guilherme.5337@gmail.com>
2021-02-23 00:54:40 -0300

    Creation of entitie Department

--- src/app/App.java
+++ src/app/App.java
@@ -1,31 +1,10 @@
 package app;
 
-import db.DB;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import model.entities.Department;
 
 public class App {
     public static void main(String[] args) {
-        Connection connection;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DB.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select * from department");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("id") + ", " + resultSet.getString("name"));
-            }
-        } catch (SQLException reject) {
-            reject.printStackTrace();
-        } finally {
-            DB.closeResultSet(resultSet);
-            DB.closeStatement(statement);
-            DB.closeConnection();
-        }
-        DB.closeConnection();
+        Department department = new Department(1, "Electronics");
+        System.out.println(department);
     }
 }
