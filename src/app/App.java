f4980b9562504afd7fce0964653985494504232a
Guilhermino <Guilherme.5337@gmail.com>
2021-02-24 23:34:43 -0300

    Creation of entity Seller

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,14 @@
 package app;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
         Department department = new Department(1, "Electronics");
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000D, 1);
         System.out.println(department);
     }
 }
